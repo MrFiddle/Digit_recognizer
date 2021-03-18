6904fb8660c130e2fd8bbe62ed1045dcdd27c059
Feli <a01023976@itesm.mx>
2021-03-18 14:04:58 -0600

    Quité el canny de predictor porque eso se hace antes

--- Predictor.py
+++ Predictor.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
 import numpy as np
-import cv2
 
 model = tf.keras.models.load_model('CNN.h5')
 def predice(entrada):
-    
-    entrada = cv2.Canny(entrada, 100, 200)
     entrada = entrada.astype("float32") / 255
     img = np.resize(entrada, (28,28,1))
     im2arr = np.array(img)
