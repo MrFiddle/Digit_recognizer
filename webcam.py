cc642bbb619486c4f5db1cbaead1d0d189bc5f28
BrandonMagana <brandonmm1208@outlook.com>
2021-03-18 15:34:04 -0600

    Webcam, que captura foto(input), para ser procesada por la red neuronal

--- webcam.py
+++ webcam.py
@@ -1,8 +1,20 @@
+from PIL import Image
 import cv2
 import os
+
+def center_crop(img,dim):
+    #Recorta la imagen, acorde a las dimesiones indicadas
+    ancho, alto = img.shape[1], img.shape[0]
+    crop_ancho = dim[0] if dim[0]<img.shape[1] else img.shape[1]
+    crop_alto = dim[1] if dim[1]<img.shape[0] else img.shape[0]
+    mid_x, mid_y = int(ancho/2), int(alto/2)
+    crop_ancho2, crop_alto2 = int(crop_ancho/2), int(crop_alto/2) 
+    cropped = img[mid_y-crop_alto2:mid_y+crop_alto2, mid_x-crop_ancho2:mid_x+crop_ancho2]
+    return cropped
+
 def cam():
+    #Abrimos Camara y  tomamos captura, regresamos la imagen
     cam = cv2.VideoCapture(0)
-    img_counter = 0
     cwd=os.getcwd()
     test=os.listdir()
 
@@ -17,16 +29,30 @@ def cam():
             break
         cv2.imshow("Prueba. Presiona " "Space" " para tomar imagen", frame)
 
-        key = cv2.waitKey(1)
-        if key%256 == 32:
-            # SPACE pressed
-            img_name = "imagen.png".format(img_counter)
+        tecla = cv2.waitKey(1)
+        if tecla%256 == 32:
+            #Space presionado, procede a tomar captura y guarda imagen
+            img_name = "imagen.png"
             cv2.imwrite(img_name, frame)
+            image=cv2.imread("imagen.png")
+            dim=(300,300)
+            cropped=center_crop(image,dim)
+            cropped=cv2.cvtColor(cropped,cv2.COLOR_BGR2GRAY)
+            cv2.imwrite("imagen.png",cropped)
             print("{} ha sido capturada!".format(img_name))
             break
     cam.release()
     cv2.destroyAllWindows()
     return "imagen.png"
 
+def pixel(image):
+    #Convierte imagen en resolución 28*28 pix, con la que trabajara la red neuronal
+    img = Image.open(image)
+    imgSmall = img.resize((28,28),resample=Image.BILINEAR)
+    result = imgSmall.resize(img.size,Image.NEAREST)
+    result.save('resultado.png')
+
 if __name__ == "__main__":
-    cam()
+    image=cam()
+    pixel(image)
+
