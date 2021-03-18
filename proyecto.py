c303463a2c132c6442f041af7f9dcffa80160c83
mrfiddle <jppd0657@gmail.com>
2021-03-18 12:16:32 -0700

    Cambios gráficos a la UI

--- proyecto.py
+++ proyecto.py
@@ -4,7 +4,7 @@ from tkinter import messagebox
 main_w = Tk()
 
 #Config the window app
-main_w.geometry('500x500') # Size
+main_w.geometry('800x500') # Size
 main_w.configure(bg = 'white') # Background color
 main_w.title('Proyecto | Computer Vision') # Title
 
@@ -14,6 +14,35 @@ def hola():
     msg = messagebox.showinfo("Botón", "Button test")
     
 #Botones
-B = Button(main_w, text = "Boton", command = lambda: hola()).place(x = 50, y = 50)
+B = Button(
+    
+    main_w,
+    text = "Tomar foto",
+    command = lambda: hola(),
+    width= 50,
+    justify = CENTER,
+    relief = FLAT,
+    font = "Arial",
+
+    activebackground = "WHITE",
+    activeforeground = "BLACK",
+
+    background = "BLACK",
+    foreground = "WHITE",
+    
+    ).place(x = 170, y = 250)
+
+#Labels
+
+L = Label(
+    main_w,
+    text = "Clasificador de números con bordes",
+    bg = "white",
+    pady = 40,
+    font = ("Bebas Neue", 30)
+)
+
+
+L.pack()
 
 main_w.mainloop()
