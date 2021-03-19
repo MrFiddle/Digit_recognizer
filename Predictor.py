712b9e9981e0ed401498bdc23bc9b4127f15b358
mrfiddle <jppd0657@gmail.com>
2021-03-18 22:41:50 -0700

    rezando para que ya jale

--- Predictor.py
+++ Predictor.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
+from os import XATTR_CREATE
+import PIL
 from PIL.Image import Image
+from tensorflow import keras
 import tensorflow as tf
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 model = tf.keras.models.load_model('CNN.h5')
-def predice(entrada):
+def predice(i):
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    entrada = cv2.bitwise_not(x_test[i])
     entrada = entrada.astype("float32") / 255
     img = np.resize(entrada, (28,28,1))
     im2arr = np.array(img)
     im2arr = im2arr.reshape(1,28,28,1)
     y_pred = model.predict_classes(im2arr)
     print(y_pred)
+    plt.imshow(x_test[i], cmap='gray')
+    return y_pred
 
-Image = cv2.imread("bordes.png")
-predice(Image)
+#Image = cv2.imread("bordes.png")
+#predice(Image)
